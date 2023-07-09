1349205a8d7d4ec5f3681684f8be086fd8754423
kishor <thaunnakishor@gmail.com>
2023-07-09 23:00:49 +0545

    Modular coding for Preparing Base Model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from CNNclassifier import logger
 from CNNclassifier.pipeline.stage_01_data_ingesation import DataIngestionTrainingPipeline
+from CNNclassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 
@@ -9,6 +10,19 @@ try:
     obj=DataIngestionTrainingPipeline()
     obj.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=====x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME="Prepare Base Model"
+try:
+    logger.info(f"*************")
+    logger.info(f">>>>> Stage {STAGE_NAME} Started")
+    obj=PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> Stage {STAGE_NAME} Finished")
+
 except Exception as e:
     logger.exception(e)
     raise e
