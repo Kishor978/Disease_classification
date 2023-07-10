e9f06baca69b7cb004692c7dd892d67d9e174287
kishor <thaunnakishor@gmail.com>
2023-07-11 00:42:35 +0545

    Training completed in modular form

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from CNNclassifier import logger
 from CNNclassifier.pipeline.stage_01_data_ingesation import DataIngestionTrainingPipeline
 from CNNclassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from CNNclassifier.pipeline.stage_03_training import ModelTrainingPipeline
 
 
 
@@ -26,3 +27,19 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+
+STAGE_NAME= "Training"
+if __name__== "__main__":
+    try:
+        logger.info(f"*************")
+        logger.info(f">>>>> Stage {STAGE_NAME} Started")
+        obj=ModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>> Stage {STAGE_NAME} Finished")
+
+    except Exception as e:
+        logger.exception(e)
+        raise e
